a68561d76709a9d2f6f2faed449b23118bdcd9f7
Luis Concha <lconcha@gmail.com>
2024-01-19 09:42:25 -0600

    added pial

--- cortical_streamlines.py
+++ cortical_streamlines.py
@@ -17,24 +17,31 @@ import numpy as np
 import sys
 from streamtracer import StreamTracer, VectorGrid
 
-in_surf = sys.argv[1]           # Should be a .gii file in scanner coordinates
-in_vec = sys.argv[2]            # .nii 4D
-nsteps = sys.argv[3]            # 200
-step_size = sys.argv[4]         # 0.1
-out_tck = sys.argv[5]
+in_surf      = sys.argv[1]           # Should be a .gii file in scanner coordinates
+in_surf_pial = sys.argv[2]           # Should be a .gii file in scanner coordinates
+in_vec       = sys.argv[3]            # .nii 4D
+nsteps       = sys.argv[4]            # 200
+step_size    = sys.argv[5]         # 0.1
+out_tck      = sys.argv[6]
 
 nsteps    = int(nsteps)
 step_size = float(step_size)
 
 print(f'Starting the creation of streamlines with the following arguments:')
-print(f'  - in_surf    : {in_surf}')
-print(f'  - in_vec     : {in_vec}')
-print(f'  - nsteps     : {nsteps}')
-print(f'  - step_size  : {step_size}')
-print(f'  - out_tck    : {out_tck}')
+print(f'  - in_surf      : {in_surf}')
+print(f'  - in_surf_pial : {in_surf_pial}')
+print(f'  - in_vec       : {in_vec}')
+print(f'  - nsteps       : {nsteps}')
+print(f'  - step_size    : {step_size}')
+print(f'  - out_tck      : {out_tck}')
 
 
-
+if in_surf_pial == 'no' :
+    connect_to_pial = False
+    print('    Will NOT connect pial and white surfaces. Streamlines will begin at white suerface.')
+else :
+    connect_to_pial = True
+    print('    Streamlines will begin at pial surface')
 
 # load data
 surf = nib.load(in_surf)
@@ -71,11 +78,39 @@ grid_spacing = [1, 1, 1]
 grid = VectorGrid(volvecnorm, grid_spacing)
 seeds = V2
 tracer.trace(seeds, grid, direction=1)
+print(f'   ... finished tracing.')
+
+
+# prepend the coordinate for pial surface before streamline
+if connect_to_pial :
+    print(f'   Connecting WM streamlines to pial-white lines.')
+    # Deal with the pial surface
+    print(f'  Load {in_surf_pial}')
+    surf_pial = nib.load(in_surf_pial)
+    Vpial = surf_pial.get_arrays_from_intent('NIFTI_INTENT_POINTSET')[0].data
+    Fpial = surf_pial.get_arrays_from_intent('NIFTI_INTENT_TRIANGLE')[0].data
+
+    # transform vertex coordinates
+    V2pial = Vpial.copy()
+    nvertices = Vpial.shape[0]
+    for vindex in range(nvertices):
+        thisv = Vpial[vindex,:];
+        thisv = thisv - vec.affine[:3,3].T
+        thisvpad = np.append(thisv,1)
+        thisvpadtransformed = thisvpad.dot(vec.affine)
+        V2pial[vindex,:] = thisvpadtransformed[0:3]
+        xyz_pial = V2pial[vindex]
+        xyz_streamline = tracer.xs[vindex]
+        xyz_both = np.insert(xyz_streamline,0,xyz_pial,axis=0)
+        #print(f'xyz_pial : {xyz_pial}, nvertices was {len(xyz_streamline)}, and now is {len(xyz_both)}')
+        #print(f' v0 : {xyz_both[0,]}')
+        tracer.xs[vindex] = xyz_both
+    
 
 print(f'  saving tck : {out_tck}')
 laplacian_streamlines = nib.streamlines.tractogram.Tractogram(streamlines=tracer.xs)
 laplacian_streamlines.affine_to_rasmm = vec.affine
 nib.streamlines.save(laplacian_streamlines, out_tck )
 
-print(f'  You can check the output with:\n    mrview {in_vec} -tractography.load {out_tck}')
+#print(f'  You can check the output with:\n    mrview {in_vec} -tractography.load {out_tck}')
 print('done.')
--- test_cortical_streamlines.sh
+++ test_cortical_streamlines.sh
@@ -6,8 +6,9 @@ sID=sub-74277
 hemi=lh
 nsteps=200
 step_size="0.1"
-surf_type=white
-target_type=fsaverage5
+tck_step_size=0.5
+#surf_type=white
+target_type=fsLR-32k
 doSimplify=1
 
 ### Laplacian field
@@ -35,16 +36,21 @@ fi
 if [ $doSimplify -eq 1 ]
 then
     ### Simplify the surface
-    echolor yellow "[INFO] Simplify the ${surf_type} surface to $target_type"
-    my_do_cmd resample_surface.sh \
-    $sID \
-    $hemi \
-    $surf_type \
-    $target_type
-    surf=${SUBJECTS_DIR}/${sID}/surf/${hemi}_${surf_type}_${target_type}.surf.gii
+    for surf_type in white pial
+    do
+      echolor yellow "[INFO] Simplify the ${surf_type} surface to $target_type"
+      my_do_cmd resample_surface.sh \
+      $sID \
+      $hemi \
+      $surf_type \
+      $target_type
+    done
+    surf=${SUBJECTS_DIR}/${sID}/surf/${hemi}_white_${target_type}.surf.gii
+    surf_pial=${SUBJECTS_DIR}/${sID}/surf/${hemi}_pial_${target_type}.surf.gii
 else
     echolor yellow "[INFO] Not simplifying surface"
     surf=${SUBJECTS_DIR}/${sID}/surf/${hemi}.white
+    surf_pial=${SUBJECTS_DIR}/${sID}/surf/${hemi}.pial
     target_type="fsnative"
 fi
 
@@ -54,25 +60,36 @@ fi
  echolor yellow "[INFO] Computing streamlines"
 
 
-# convert surfaces to scanner coordinates
-my_do_cmd mris_convert --to-scanner \
-  $surf \
-  ${SUBJECTS_DIR}/${sID}/surf/${hemi}_${surf_type}_${target_type}_scanner.surf.gii
-surf=${SUBJECTS_DIR}/${sID}/surf/${hemi}_${surf_type}_${target_type}_scanner.surf.gii
+# # convert surfaces to scanner coordinates
+# my_do_cmd mris_convert --to-scanner \
+#   $surf \
+#   ${SUBJECTS_DIR}/${sID}/surf/${hemi}_white_${target_type}_scanner.surf.gii
+# surf=${SUBJECTS_DIR}/${sID}/surf/${hemi}_white_${target_type}_scanner.surf.gii
+# my_do_cmd mris_convert --to-scanner \
+#   $surf_pial \
+#   ${SUBJECTS_DIR}/${sID}/surf/${hemi}_pial_${target_type}_scanner.surf.gii
+# surf_pial=${SUBJECTS_DIR}/${sID}/surf/${hemi}_pial_${target_type}_scanner.surf.gii
 
+# echolor orange "Check: freeview -f $surf $surf_pial"
+# exit 2
 
 #in_surf=${SUBJECTS_DIR}/${sID}/surf/${hemi}_white_scanner.surf.gii
 in_surf=$surf
 in_vec=${SUBJECTS_DIR}/${sID}/mri/laplace-wm_vec.nii.gz
-out_tck=${SUBJECTS_DIR}/${sID}/mri/${hemi}_${surf_type}_${target_type}_laplace-wm-streamlines.tck
+tmp_tck=/tmp/temp_$$.tck
+out_tck=${SUBJECTS_DIR}/${sID}/mri/${hemi}_${target_type}_laplace-wm-streamlines.tck
 my_do_cmd python cortical_streamlines.py \
   $in_surf \
+  $surf_pial \
   $in_vec \
   $nsteps \
   $step_size \
-  $out_tck
+  $tmp_tck
+my_do_cmd tckresample -force -quiet -step_size $tck_step_size $tmp_tck $out_tck
+rm $tmp_tck
 
 
+my_do_cmd tckresample -force -quiet -endpoints $out_tck ${out_tck%.tck}_orig.tck
 my_do_cmd tckresample -force -quiet -endpoints $out_tck ${out_tck%.tck}_endsOnly.tck
 
 echolor cyan "mrview ${SUBJECTS_DIR}/${sID}/mri/aparc+aseg.nii.gz ${SUBJECTS_DIR}/${sID}/mri/laplace-wm_vec.nii.gz \
