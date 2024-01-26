9d12ee8d6f00decca974639997c53fee8084102e
Luis Concha <lconcha@gmail.com>
2024-01-25 18:07:58 -0600

    first, and wrong

---
+++ test_tckfixelsample.m
@@ -0,0 +1,7 @@
+
+
+f_fa  = '/datos/syphon/tmp/test_cortical/fa.nii.gz'
+f_tck = '/datos/syphon/tmp/test_cortical/lh_fsLR-32k_laplace-wm-streamlines_dwispace.tck'
+f_prefix = '/datos/syphon/tmp/test_cortical/test'
+
+cortical_tckfixelsample(f_tck, f_fa, f_fa, f_fa, f_prefix)
