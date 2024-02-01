f9ae5e46e863bafd8521230c296ec61a4026ce22
Luis Concha <lconcha@gmail.com>
2024-02-01 11:03:22 -0600

    not finished

--- cortical_par-perp_sampling.sh
+++ cortical_par-perp_sampling.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 source `which my_do_cmd`
 
+# Use my custom-made mrtrix function
+# must download and compile to $mymrtrix directory
+# https://github.com/lconcha/mrtrix3 
+mymrtrix=/misc/lauterbur/lconcha/code/mrtrix3
+tckfixeldots=${mymrtrix}/bin/tckfixeldots
+
 sID=$1
 surf_type=$2
 
@@ -22,8 +28,6 @@ fi
 
 tck_lh=${SUBJECTS_DIR}/${sID}/dwi/lh_${surf_type}_laplace-wm-streamlines_dwispace.tck
 tck_rh=${SUBJECTS_DIR}/${sID}/dwi/rh_${surf_type}_laplace-wm-streamlines_dwispace.tck
-#mrds_PDD=${SUBJECTS_DIR}/${sID}/dwi/${sID}_MRDS_Diff_BIC_PDDs_CARTESIAN.nii.gz
-
 
 isOK=1
 for f in $tck_lh $tck_rh $mrds_PDD
@@ -39,11 +43,4 @@ done
 if [ $isOK -eq 0 ]; then exit 2; fi
 
 
-csd_fixeldir=${SUBJECTS_DIR}/${sID}/dwi/csd_fixels
-my_do_cmd fixel2voxel \
-  ${csd_fixeldir}/afd_fixels.nii.gz \
-  none \
-  ${csd_fixeldir}/afd_voxel.nii.gz
 
-
-#peaks2fixel $mrds_PDD $mrds_fixeldir
