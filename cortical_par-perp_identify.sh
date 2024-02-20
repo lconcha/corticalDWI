9af1d4311000ecf81fbd1a1b680b909b0c579b7b
Luis Concha <lconcha@gmail.com>
2024-02-20 15:25:00 -0600

    added max_angle

--- cortical_par-perp_identify.sh
+++ cortical_par-perp_identify.sh
@@ -9,6 +9,7 @@ tckfixeldots=${mymrtrix}/bin/tckfixeldots
 
 sID=$1
 surf_type=$2
+max_angle=60
 
 if [ ! -d ${SUBJECTS_DIR}/${sID} ]
 then
@@ -41,12 +42,12 @@ if [ $isOK -eq 0 ]; then exit 2; fi
 echolor cyan "[INFO] Identifying fixels for CSD"
 tsf_lh_csd=${SUBJECTS_DIR}/${sID}/dwi/lh_${surf_type}_laplace-wm-streamlines_dwispace_parIndices_csd.tsf
 tsf_rh_csd=${SUBJECTS_DIR}/${sID}/dwi/rh_${surf_type}_laplace-wm-streamlines_dwispace_parIndices_csd.tsf
-my_do_cmd $tckfixeldots $fixels_csd $tck_lh $tsf_lh_csd
-my_do_cmd $tckfixeldots $fixels_csd $tck_rh $tsf_rh_csd
+my_do_cmd $tckfixeldots -angle $max_angle $fixels_csd $tck_lh $tsf_lh_csd
+#my_do_cmd $tckfixeldots -angle $max_angle $fixels_csd $tck_rh $tsf_rh_csd
 
 echolor cyan "[INFO] Identifying fixels for MRDS"
 tsf_lh_mrds=${SUBJECTS_DIR}/${sID}/dwi/lh_${surf_type}_laplace-wm-streamlines_dwispace_parIndices_mrds.tsf
 tsf_rh_mrds=${SUBJECTS_DIR}/${sID}/dwi/rh_${surf_type}_laplace-wm-streamlines_dwispace_parIndices_mrds.tsf
-my_do_cmd $tckfixeldots $fixels_mrds $tck_lh $tsf_lh_mrds
-my_do_cmd $tckfixeldots $fixels_mrds $tck_rh $tsf_rh_mrds
+#my_do_cmd $tckfixeldots -angle $max_angle $fixels_mrds $tck_lh $tsf_lh_mrds
+#my_do_cmd $tckfixeldots -angle $max_angle $fixels_mrds $tck_rh $tsf_rh_mrds
 
