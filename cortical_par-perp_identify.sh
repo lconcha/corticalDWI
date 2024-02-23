e96da672717c0f5e05927c2b6e5670cbda36ed3a
Luis Concha <lconcha@gmail.com>
2024-02-23 15:06:31 -0600

    working

--- cortical_par-perp_identify.sh
+++ cortical_par-perp_identify.sh
@@ -21,7 +21,7 @@ fi
 
 tck_lh=${SUBJECTS_DIR}/${sID}/dwi/lh_${surf_type}_laplace-wm-streamlines_dwispace.tck
 tck_rh=${SUBJECTS_DIR}/${sID}/dwi/rh_${surf_type}_laplace-wm-streamlines_dwispace.tck
-fixels_csd=${SUBJECTS_DIR}/${sID}/dwi/csd_fixels/directions.mif
+fixels_csd=${SUBJECTS_DIR}/${sID}/dwi/csd_fixels/afd_fixels.nii.gz
 fixels_mrds=${SUBJECTS_DIR}/${sID}/dwi/mrds_fixels/directions.mif
 
 isOK=1
@@ -43,11 +43,16 @@ echolor cyan "[INFO] Identifying fixels for CSD"
 tsf_lh_csd=${SUBJECTS_DIR}/${sID}/dwi/lh_${surf_type}_laplace-wm-streamlines_dwispace_parIndices_csd.tsf
 tsf_rh_csd=${SUBJECTS_DIR}/${sID}/dwi/rh_${surf_type}_laplace-wm-streamlines_dwispace_parIndices_csd.tsf
 my_do_cmd $tckfixeldots -angle $max_angle $fixels_csd $tck_lh $tsf_lh_csd
-#my_do_cmd $tckfixeldots -angle $max_angle $fixels_csd $tck_rh $tsf_rh_csd
+my_do_cmd $tckfixeldots -angle $max_angle $fixels_csd $tck_rh $tsf_rh_csd
 
 echolor cyan "[INFO] Identifying fixels for MRDS"
 tsf_lh_mrds=${SUBJECTS_DIR}/${sID}/dwi/lh_${surf_type}_laplace-wm-streamlines_dwispace_parIndices_mrds.tsf
 tsf_rh_mrds=${SUBJECTS_DIR}/${sID}/dwi/rh_${surf_type}_laplace-wm-streamlines_dwispace_parIndices_mrds.tsf
-#my_do_cmd $tckfixeldots -angle $max_angle $fixels_mrds $tck_lh $tsf_lh_mrds
-#my_do_cmd $tckfixeldots -angle $max_angle $fixels_mrds $tck_rh $tsf_rh_mrds
+my_do_cmd $tckfixeldots -angle $max_angle $fixels_mrds $tck_lh $tsf_lh_mrds
+my_do_cmd $tckfixeldots -angle $max_angle $fixels_mrds $tck_rh $tsf_rh_mrds
 
+
+echo mrview ${SUBJECTS_DIR}/${sID}/dwi/fa.nii.gz \
+  -tractography.load $tck_lh \
+  -tractography.tsf_load $tsf_lh_csd \
+  -tractography.tsf_thresh -1,0.1
