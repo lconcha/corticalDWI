234fab585f4bb315e738b99a1892c7cf8ff9e3c9
Luis Concha <lconcha@gmail.com>
2025-01-08 10:26:11 -0600

    added help to view results

--- cortical_tck2fixeldots.sh
+++ cortical_tck2fixeldots.sh
@@ -7,7 +7,7 @@ target_type=$3
 fixel_dir=$4; # csd_fixels or mrds_fixels (or something else)
 
 
-tck=${SUBJECTS_DIR}/${subjID}/dwi/${hemi}_fsLR-${target_type}_laplace-wm-streamlines_dwispace.tck
+tck=${SUBJECTS_DIR}/${subjID}/dwi/${hemi}_${target_type}_laplace-wm-streamlines_dwispace.tck
 fixel_dir=${SUBJECTS_DIR}/${subjID}/dwi/${fixel_dir}
 dots=${hemi}_fsLR_${target_type}_fixeldots.mif
 
@@ -28,6 +28,8 @@ fi
 if [ -f ${fixel_dir}/${dots} ]
 then
   echolor orange "[ERROR] File exists. Not overwriting: ${fixel_dir}/${dots}"
+  echolor green "[INFO] Check result with:"
+  echolor green "       mrview ${subjID}/dwi/fa.nii.gz -tractography.load $tck -tractography.colour 0,0,1 -fixel.load ${fixel_dir}/${dots}"
   exit 0
 fi
 
@@ -40,3 +42,8 @@ my_do_cmd  tck2fixeldots \
     $fixel_dir \
     $dots
 fi
+
+echolor green "[INFO] Check result with:"
+echolor green "       mrview ${subjID}/dwi/fa.nii.gz \
+                        -tractography.load $tck \
+                        -fixel.load ${fixel_dir}/${dots}"
