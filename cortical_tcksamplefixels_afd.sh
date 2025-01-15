3fc7b3452248c133c5abbbd6fb1a1eee7be67100
Luis Concha <lconcha@gmail.com>
2025-01-14 20:02:02 -0600

    do both hemispheres and target_type at the same time in a loop

--- cortical_tcksamplefixels_afd.sh
+++ cortical_tcksamplefixels_afd.sh
@@ -2,10 +2,8 @@
 source `which my_do_cmd`
 
 subjID=$1
-hemi=$2
-target_type=$3; # fsLR-5k or fsLR-32k
-fixel_dir=$4; # csd_fixels or mrds_fixels (or something else)
-angle=$5
+fixel_dir=$2; # csd_fixels or mrds_fixels (or something else)
+angle=$3
 
 
 
@@ -20,40 +18,47 @@ then
 fi
 
 
-tck=${SUBJECTS_DIR}/${subjID}/dwi/${hemi}_${target_type}_laplace-wm-streamlines_dwispace.tck
-afd=${fixel_dir}/afd_fixels.mif
-for f in $tck $afd
+for hemi in lh rh
 do
-  if [ ! -f $f ]
-  then
-    echolor red "[ERROR] Cannot find file: $f"
-    isOK=0
-  else
-    echolor green "[INFO] Found file: $f"
-  fi
+  for target_type in fsLR-5k fsLR-32k
+  do
+
+    tck=${SUBJECTS_DIR}/${subjID}/dwi/${hemi}_${target_type}_laplace-wm-streamlines_dwispace.tck
+    afd=${fixel_dir}/afd_fixels.mif
+    for f in $tck $afd
+    do
+      if [ ! -f $f ]
+      then
+        echolor red "[ERROR] Cannot find file: $f"
+        isOK=0
+      else
+        echolor green "[INFO] Found file: $f"
+      fi
+    done
+
+
+    fcheck=${fixel_dir}/${hemi}_${target_type}_afd-par-perp-indices.tsf
+    echo "looking for $fcheck"
+    if [ -f $fcheck ]
+    then
+      echolor yellow "[WARN] File exists, will not overwrite: $fcheck"
+      exit 0
+    fi
+
+
+    if [ $isOK -eq 1 ]
+    then
+      my_do_cmd tcksamplefixels \
+      -angle $angle \
+      $afd \
+      $tck \
+      ${fixel_dir}/${hemi}_${target_type}_afd-par-perp-indices.tsf \
+      ${fixel_dir}/${hemi}_${target_type}_afd-par.tsf \
+      ${fixel_dir}/${hemi}_${target_type}_afd-perp.tsf \
+      ${fixel_dir}/${hemi}_${target_type}_afd-perp-av.tsf
+    else
+      echolor red "[ERROR] Cannot continue, see above errors"
+      exit 2
+    fi
+  done
 done
-
-
-fcheck=${fixel_dir}/${hemi}_${target_type}_afd-par-perp-indices.tsf
-echo "looking for $fcheck"
-if [ -f $fcheck ]
-then
-  echolor yellow "[WARN] File exists, will not overwrite: $fcheck"
-  exit 0
-fi
-
-
-if [ $isOK -eq 1 ]
-then
-  my_do_cmd tcksamplefixels \
-  -angle $angle \
-  $afd \
-  $tck \
-  ${fixel_dir}/${hemi}_${target_type}_afd-par-perp-indices.tsf \
-  ${fixel_dir}/${hemi}_${target_type}_afd-par.tsf \
-  ${fixel_dir}/${hemi}_${target_type}_afd-perp.tsf \
-  ${fixel_dir}/${hemi}_${target_type}_afd-perp-av.tsf
-else
-  echolor red "[ERROR] Cannot continue, see above errors"
-  exit 2
-fi
