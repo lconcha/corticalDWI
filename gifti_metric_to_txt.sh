3d3a5bacc273c6951ef9e1cc0158cd1ebf23d252
Luis Concha <lconcha@gmail.com>
2025-04-10 10:05:38 -0600

    added info

--- gifti_metric_to_txt.sh
+++ gifti_metric_to_txt.sh
@@ -8,6 +8,7 @@ wb_command -gifti-convert \
   ASCII $gifti \
   ${gifti%.gii}.xml
 
+# This extracts ony the data from the bit xml file
 awk '/<Data>/{flag=1; next} /<\/Data>/{flag=0} flag' \
  ${gifti%.gii}.xml  | \
  awk '{print $1}' > $txt
