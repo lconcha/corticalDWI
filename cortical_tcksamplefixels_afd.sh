3b711f5eb90651b4c6cafda10f651bb128e1d944
Luis Concha <lconcha@gmail.com>
2025-05-30 13:02:05 -0600

    removed target_type loop, now must specify as argument

--- cortical_tcksamplefixels_afd.sh
+++ cortical_tcksamplefixels_afd.sh
@@ -3,7 +3,7 @@ source `which my_do_cmd`
 
 help() {
   echo "
-  Usage: $(basename $0) <subjID> <fixel_dir> <angle> <nDepths>
+  Usage: $(basename $0) <subjID> <fixel_dir> <angle> <nDepths> <target_type>
   
   <subjID>     subject ID in the form of sub-74277
   <fixel_dir>  directory containing fixel files, e.g., csd_fixel
@@ -12,6 +12,7 @@ help() {
   <nDepths>    number of depth points to keep in the txt file.
                This is in steps, not mm,
                and has to be less than or equal to the number of depth points in the tsf file.
+  <target_type> fsLR-5k or fsLR-32k
 
   Only AFD values are sampled, and separated into parallel and perpendicular components.
   Perpendicular can be defined in two ways:
@@ -20,7 +21,7 @@ help() {
   "
 }
 
-if [ $# -ne 4 ]
+if [ $# -ne 5 ]
 then
   echolor red "Wrong number of arguments"
   help
@@ -32,7 +33,7 @@ subjID=$1
 fixel_dir=$2; # csd_fixels
 angle=$3
 nDepths=$4; # number of depth points to keep in the txt file. The tsf saves them all.
-
+target_type=$5 # fsLR-5k or fsLR-32k
 
 isOK=1
 
@@ -47,47 +48,46 @@ fi
 
 for hemi in lh rh
 do
-  for target_type in fsLR-5k fsLR-32k
+
+
+  tck=${SUBJECTS_DIR}/${subjID}/dwi/${hemi}_${target_type}_laplace-wm-streamlines_dwispace.tck
+  afd=${fixel_dir}/afd_fixels.mif
+  for f in $tck $afd
   do
-
-    tck=${SUBJECTS_DIR}/${subjID}/dwi/${hemi}_${target_type}_laplace-wm-streamlines_dwispace.tck
-    afd=${fixel_dir}/afd_fixels.mif
-    for f in $tck $afd
-    do
-      if [ ! -f $f ]
-      then
-        echolor red "[ERROR] Cannot find file: $f"
-        isOK=0
-      else
-        echolor green "[INFO] Found file: $f"
-      fi
-    done
-
-
-    fcheck=${fixel_dir}/${hemi}_${target_type}_afd-par-perp-indices.tsf
-    echo "looking for $fcheck"
-    if [ -f $fcheck ]
+    if [ ! -f $f ]
     then
-      echolor yellow "[WARN] File exists, will not overwrite: $fcheck"
-      exit 0
-    fi
-
-
-    if [ $isOK -eq 1 ]
-    then
-      my_do_cmd tcksamplefixels \
-      -angle $angle \
-      $afd \
-      $tck \
-      ${fixel_dir}/${hemi}_${target_type}_afd-par-perp-indices.tsf \
-      ${fixel_dir}/${hemi}_${target_type}_afd-par.tsf \
-      ${fixel_dir}/${hemi}_${target_type}_afd-perp.tsf \
-      ${fixel_dir}/${hemi}_${target_type}_afd-perp-av.tsf
+      echolor red "[ERROR] Cannot find file: $f"
+      isOK=0
     else
-      echolor red "[ERROR] Cannot continue, see above errors"
-      exit 2
+      echolor green "[INFO] Found file: $f"
     fi
   done
+
+
+  fcheck=${fixel_dir}/${hemi}_${target_type}_afd-par-perp-indices.tsf
+  echo "looking for $fcheck"
+  if [ -f $fcheck ]
+  then
+    echolor yellow "[WARN] File exists, will not overwrite: $fcheck"
+    exit 0
+  fi
+
+
+  if [ $isOK -eq 1 ]
+  then
+    my_do_cmd tcksamplefixels \
+    -angle $angle \
+    $afd \
+    $tck \
+    ${fixel_dir}/${hemi}_${target_type}_afd-par-perp-indices.tsf \
+    ${fixel_dir}/${hemi}_${target_type}_afd-par.tsf \
+    ${fixel_dir}/${hemi}_${target_type}_afd-perp.tsf \
+    ${fixel_dir}/${hemi}_${target_type}_afd-perp-av.tsf
+  else
+    echolor red "[ERROR] Cannot continue, see above errors"
+    exit 2
+  fi
+  
 done
 
 
@@ -97,4 +97,4 @@ done
 #   my_do_cmd  cortical_tsf2txt_matlab.sh $tsf $txt $nDepths
 # done
 
-cortical_tsf2txt_matlab.sh $fixel_dir $nDepths
+my_do_cmd cortical_tsf2txt_matlab.sh $fixel_dir $nDepths
