0f9bf0ff4713bf19cf0a32818f4257b4cdda56d1
Luis Concha <lconcha@gmail.com>
2025-06-02 17:39:30 -0600

    typos

--- cortical_multi-stats_per_region.sh
+++ cortical_multi-stats_per_region.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+source `which my_do_cmd`
 
-sID=$1
-target_type=$2
 f_template=$(dirname $0)/cortical_multi-stats_per_region_template.txt
 
 help() {
   echo "
-  Usage: $(basename $0) <subjID> <target_type> [options]
+  Usage: $(basename $0)  <subjID> <target_type> [options]
 
   <subjID>         subject ID in the form of sub-74277
   <target_type>    type of target (e.g., 'fsLR-32k')
@@ -28,11 +27,12 @@ then
 fi
 
 
-while getopts "f" opt
+while getopts "t:" opt
 do
   case $opt in
     t)
       f_template=${OPTARG}
+      echolor green "[INFO] Using template file: $f_template"
     ;;
     \?)
       echolor red "[ERROR] Invalid flag."
@@ -40,7 +40,20 @@ do
     ;;
   esac
 done
-shift $((OPTIND-1))
+shift "$((OPTIND-1))"
+
+sID=$1
+target_type=$2
+
+
+
+if [ ! -f $f_template ]; then
+  echolor red "[ERROR] Template file not found: $f_template"
+  exit 1
+fi
+
+
+echolor green "[INFO] Processing subject: $sID with target type: $target_type"
 
 
 cat $f_template | while read line
@@ -50,7 +63,7 @@ do
     do
         f=$(echo $line | awk '{print $1}' | sed "s/HEMI/${hemi}/" | sed "s/TARGET/${target_type}/")
         metric_name=$(echo $line | awk '{print $2}')
-        cortical_stats_per_region.sh $sID $hemi $target_type $f $metric_name
+        my_do_cmd  cortical_stats_per_region.sh $sID $hemi $target_type $f $metric_name
     done
 done
 
