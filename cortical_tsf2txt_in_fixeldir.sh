a3ad2c3aa1af9e0c4131ab707bb785977553d6f6
Luis Concha <lconcha@gmail.com>
2025-06-05 09:42:13 -0600

    not using nDepths anymore

--- cortical_tsf2txt_in_fixeldir.sh
+++ cortical_tsf2txt_in_fixeldir.sh
@@ -6,14 +6,11 @@ thispath=$(dirname $0)
 
 help() {
   echo "
-  Usage: $(basename $0) <fixel_dir> <nDepths>
+  Usage: $(basename $0) <fixel_dir>
   
   <fixel_dir>  directory containing fixel files, e.g., csd_fixel
-  <nDepths>    number of depth points to keep in the txt file.
-               This is in steps, not mm,
-               and has to be less than or equal to the number of depth points in the tsf file.
   
-  Converts all .tsf files in the fixel_dir to .txt files with nDepths depth points.
+  Converts all .tsf files in the fixel_dir to .txt files.
 
   "
 }
@@ -26,9 +23,13 @@ then
 fi
 
 fixel_dir=$1
-nDepths=$2
+
+
+
 
 for f in $fixel_dir/*.tsf
 do
-  cortical_tsf2txt.sh $f ${f%.tsf}.txt $nDepths
+  my_do_cmd tsf2txt $f ${f%.tsf}.txt
 done
+
+
