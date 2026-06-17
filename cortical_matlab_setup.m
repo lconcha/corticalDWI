41eca60f3340bda959e45c11c2014045e5c61872
Luis Concha <lconcha@gmail.com>
2026-06-17 13:40:37 -0600

    specify where to get dependencies

--- cortical_matlab_setup.m
+++ cortical_matlab_setup.m
@@ -3,11 +3,11 @@ function cortical_matlab_setup()
 % Edit the paths in this section to match your system.
 
 %%% Edit this section %%%
-brainstat   = '/misc/lauterbur2/lconcha/code/BrainStat/brainstat_matlab';
-gifti       = '/misc/lauterbur2/lconcha/code/gifti';
-mrtrix      = '/home/inb/soporte/lanirem_software/mrtrix_3.0.4/matlab';
+brainstat   = '/misc/lauterbur2/lconcha/code/BrainStat/brainstat_matlab'; % https://github.com/MICA-MNI/Brainstat
+gifti       = '/misc/lauterbur2/lconcha/code/gifti'; % https://github.com/gllmflndn/gifti
+mrtrix      = '/home/inb/soporte/lanirem_software/mrtrix_3.0.4/matlab'; % https://github.com/Mrtrix3/mrtrix3
 corticalDWI = '/misc/lauterbur2/lconcha/code/corticalDWI';
-cbrewer     = '/misc/lauterbur2/lconcha/code/cbrewer';
+cbrewer     = '/misc/lauterbur2/lconcha/code/cbrewer';  % https://github.com/sijiazhao/cbrewer or https://git.fmrib.ox.ac.uk/amyh/bigmacanalysis/-/tree/main/cbrewer/cbrewer?ref_type=heads
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 addpath(genpath(brainstat));
